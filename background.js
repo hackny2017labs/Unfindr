8db70fad2841ce7b18824ecc89cf655fae9cca0d
puzzledsean <puzzledsean@gmail.com>
2017-07-06 01:09:10 -0400

    basic card creation for first friend

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   });
 });
 
-// This block is new!
+// open new tab
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
--- content.js
+++ content.js
@@ -1,12 +1,42 @@
 // content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if( request.message === "clicked_browser_action" ) {
 		var facebookFriendsLink = 'https://m.facebook.com/friends/center/friends';
-      	console.log('https://m.facebook.com/friends/center/friends');
 
-		// This line is new!
+		// send a message that the user clicked on the tab
 	    chrome.runtime.sendMessage({"message": "open_new_tab", "url": facebookFriendsLink});
     }
-  }
-);
+});
+
+const friendList = Array.from(document.querySelectorAll('#friends_center_main div[data-sigil="undoable-action"]'));
+
+// get current friends listed on the page 
+var friends = []
+friendList.forEach((element) => {
+	var friendInfo = {}
+	var friendName = element.querySelector('h3 a').innerHTML;
+	friendInfo['friendName'] = friendName;
+
+	var regexImage = /\(([^)]+)\)/;
+	var imageUrlToBeCleaned = element.querySelector('div div div a i').getAttribute('style')
+	var cleanedImageUrl = regexImage.exec(imageUrlToBeCleaned)[1];
+
+	friendInfo['imageUrl'] = cleanedImageUrl;
+
+	friends.push(friendInfo);
+});
+
+// clear the viewport
+document.getElementById('viewport').style.display = "none";
+
+// add in a card
+console.log(friends[0]['imageUrl'].toString())
+var card = 
+	'<div class="card" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);transition: 0.3s;">' + 
+  	'<img src=' + friends[0]['imageUrl'].toString() + ' alt="Avatar" style="no-repeat center;background-size:100% 100%;-webkit-background-size:100% 100%;width:68px;height:68px;">' +
+  	'<div class="container style=padding: 2px 16px;">' +
+  	'<h4><b>' + friends[0]['friendName'] + '</b></h4>' +
+   	'<p>Architect & Engineer</p>'+
+  	'</div>'+ 
+	'</div>'
+document.body.innerHTML = card;
